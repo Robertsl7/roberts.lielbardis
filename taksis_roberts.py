2b02b3c910394939958c1ef11ddb43022b8b869d
Robertsl7 <144447070+Robertsl7@users.noreply.github.com>
2023-10-06 12:09:38 +0300

    Add files via upload

---
+++ taksis_roberts.py
@@ -0,0 +1,31 @@
+nolīgšana= 1,20
+dienā = 0,47
+naktī= 0,87
+gaidīšana = 0,15
+izsaukums = 2,40
+
+pasažieri = int(input('Cik pasažieri brauks ar taksi? (1 līdz 4 / vairāk kā 4): '))
+if pasažieri >4 : #pasažieru skaits tiek ierobežots līdz 4
+    print('Jūms ir pārsniegts pasažieru skaits ')
+elif pasažieri <= 4: #pasažieru skaits tiek ierobežots no 1 līdz 4
+    pulkstenis = int(input ('Cikos plānojat veikt izsaukumu? (veselās stundās) '))
+    if pulkstenis >5 or pulkstenis<23:
+       print('Par taksi šajā laika periodā būs jāmaksā 0,47 par km ')
+    elif pulkstenis <6 or pulkstenis>23:
+        print('Par taksi šajā laika periodā būs jāmaksā 0,87 par km ')
+    vieta = (input('Vai Jūs atrodaties pie mūsu firmas stāvvietas? (jā/nē) '))
+    if vieta == 'nē':
+        print('Tad Jums nāksies maksāt gan par nolīgšanas cenu, kas ir 1,20, gan par izsaukuma izsaukumu, kas ir 2,40 ')
+    elif vieta =='jā':
+        print('Tad Jums nāksies maksāt tikai par nolīgšanas cenu 1,20 ')
+        gaidīšana = (input ('Vai Jums ir vēlme veikt apstāšanos pa ceļam? (jā/nē)  '))#tiek veikta apstāšanās jautājuma uzdošana
+        if gaidīšana == 'jā':
+            print('Cik ilgi? (Par katru gaidīšanas mīnūti jāpiemaksā 0,15) ')
+        elif gaidīšana =='nē':
+            print('Tad nav jāmaksā par gaidīšanu ')
+            attālums = int(input('Cik km jābrauc līdz galapunktam (km skaits 1-20) '))
+            if attālums <=20:
+                print('Tiks veikts norādītais km daudzums')
+            
+
+   
